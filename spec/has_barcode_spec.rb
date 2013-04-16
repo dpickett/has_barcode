de47b49f3b31b9bf36fbdf0eaaf510859df25161
Satoshi SUZUKI <akn@s-satoshi.net>
2013-04-16 12:16:52 +0900

    Described reproductive code for PDF LoadError.
    now it's failed

--- spec/has_barcode_spec.rb
+++ spec/has_barcode_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/models/has_png_barcode')
 
 describe "A class that has a barcode" do
   before(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,3 @@ require 'has_barcode'
 
 require 'rspec'
 
-require "#{File.dirname(__FILE__)}/models/has_png_barcode"
