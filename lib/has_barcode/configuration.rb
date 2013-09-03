3b5cfe4c73e368258e2822fb79110fbff7d9379b
Satoshi SUZUKI <akn@s-satoshi.net>
2013-09-03 12:19:23 +0900

    fixed bugs on heroku

--- lib/has_barcode/configuration.rb
+++ lib/has_barcode/configuration.rb
@@ -7,13 +7,13 @@ module HasBarcode
       @name = args.first
 
       if opts[:type].kind_of?(String) || opts[:type].kind_of?(Symbol)
-        require "barby/barcode/#{opts[:type]}"
+        require "barby/barcode/#{opts[:type].downcase}"
         @barcode_class = Barby.const_get(ActiveSupport::Inflector.classify("#{opts[:type].to_s}"))
       else
         @barcode_class = opts[:type]
       end
 
-      outputter_name = name_from_outputter(opts[:outputter])
+      outputter_name = name_from_outputter(opts[:outputter].downcase)
       require "barby/outputter/#{outputter_name}_outputter"
       @outputter = Barby.const_get(ActiveSupport::Inflector.classify("#{outputter_name}_outputter"))
     end
